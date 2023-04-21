3f3cd3a4b9321d4645de0938c2b58e834dd0f2da
LinCoding2910 <tanguanlin908@gmail.com>
2023-04-21 21:38:47 +0800

    Second Exercise Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = (string,num) => {
+    // take the string and ouput it back the number of times provide.
+    let newString = '';
+    if (num < 0) {
+        return 'ERROR'
+    };
+    for (let timesToRepeatString = 0;timesToRepeatString < num;timesToRepeatString++) {
+        newString += string.toString()
+    };
+    return newString
 };
 
 // Do not edit below this line
